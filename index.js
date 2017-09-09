d32d374fc03f9bf69ecf6729a22cd8a918b4bea2
Taylor Dulleakellegrew <twdulleakellegrew@email.neit.edu>
2017-09-09 01:05:20 -0400

    Fixed .get not returning anything if params had incorrect case

--- index.js
+++ index.js
@@ -11,14 +11,14 @@ app.get('/', function(req, res){
 app.get('/team/:id', function(req, res){
 	let rawdata = rosterData.readFileSync('teams.json', 'utf8');
 	let teams = JSON.parse(rawdata);
-	res.send(teams[req.params.id]);
+	res.send(teams[req.params.id.toLowerCase()]);
 })
 app.get('/team/:id/:pos', function(req, res){
 	let rawdata = rosterData.readFileSync('teams.json', 'utf8');
 	let teams = JSON.parse(rawdata);
 	console.log("\"" + req.params.pos + "\"");
-	console.log(jsonQuery(req.params.id + '.Players[*Position='+ req.params.pos +'].PlayerName',{data:teams}));
-	res.status(200).send(jsonQuery(req.params.id + '.Players[*Position='+ req.params.pos +'].PlayerName',{data:teams}).value);
+	console.log(jsonQuery(req.params.id.toLowerCase() + '.Players[*Position='+ req.params.pos.toUpperCase() +'].PlayerName',{data:teams}));
+	res.status(200).send(jsonQuery(req.params.id.toLowerCase() + '.Players[*Position='+ req.params.pos.toUpperCase() +'].PlayerName',{data:teams}).value);
 })
 app.listen(3000, function(){
 	console.log('Example app listening on port 3000!')
